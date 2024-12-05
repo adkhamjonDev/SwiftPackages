96980c58c014acba22025e82346e6ea62c53c1ce
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-12-05 14:52:04 +0500

    Initial Commit

---
+++ SwiftPackages/SwiftPackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackagesApp.swift
+//  SwiftPackages
+//
+//  Created by Adkhamjon Rakhimov on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
